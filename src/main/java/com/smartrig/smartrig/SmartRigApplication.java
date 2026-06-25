89541aeac54af6b146e050bc8115c29996989a55
isyouyoung <isyouyoung@gmail.com>
2026-06-25 21:34:13 +0900

    첫 commit 프로젝트 생성

---
+++ src/main/java/com/smartrig/smartrig/SmartRigApplication.java
@@ -0,0 +1,13 @@
+package com.smartrig.smartrig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartRigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartrig/smartrig/SmartRigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartrig.smartrig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
