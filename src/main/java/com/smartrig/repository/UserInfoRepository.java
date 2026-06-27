cb3d6aba667f1659a15153adafa232525d405ce1
isyouyoung <isyouyoung@gmail.com>
2026-06-27 21:52:59 +0900

    UserInfoRepository 추가

--- src/main/java/com/smartrig/repository/UserInfoRepository.java
+++ src/main/java/com/smartrig/repository/UserInfoRepository.java
@@ -1,4 +1,15 @@
 package com.smartrig.repository;
 
-public interface UserInfoRepository {
+import com.smartrig.repository.entity.UserInfoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfoEntity, String> {
+
+    // 회원 정보 조회(회원정보 화면, 회원가입 여부 등 다양하게 활용됨)
+    Optional<UserInfoEntity> findByUserId(String userId);
+
 }
