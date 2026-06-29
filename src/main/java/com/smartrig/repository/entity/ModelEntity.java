a4a57d918bc93a7cf8d5786a81a0257830a7fbcc
isyouyoung <isyouyoung@gmail.com>
2026-06-29 15:20:18 +0900

    MODEL 테이블 Entity 매핑 추가

--- src/main/java/com/smartrig/repository/entity/ModelEntity.java
+++ src/main/java/com/smartrig/repository/entity/ModelEntity.java
@@ -1,9 +1,13 @@
 package com.smartrig.repository.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "MODEL")
 public class ModelEntity {
+
+    @Id
+    private Long modelId;
 }
