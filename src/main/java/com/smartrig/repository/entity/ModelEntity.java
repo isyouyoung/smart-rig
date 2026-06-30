2fd1b7be2ef18711b6db5dc6a9c5a9d105ee61bc
isyouyoung <isyouyoung@gmail.com>
2026-06-30 13:10:10 +0900

    주석 공부 추가

--- src/main/java/com/smartrig/repository/entity/ModelEntity.java
+++ src/main/java/com/smartrig/repository/entity/ModelEntity.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "MODEL")
+@Entity // 이 클래스가 DB와 연결되는 Entity임을 JPA에게 알려준다.
+@Table(name = "MODEL") // MODEL 테이블과 매핑한다.
 public class ModelEntity {
 
+    // MODEL 테이블의 PRIMARY KEY(model_id)와 연결된다.
+    // JPA는 이 값을 기준으로 어떤 데이터를 조회/수정/삭제할지 판단한다.
     @Id
     private Long modelId;
+
 }
