560aa1107c2ce3fd6d4a45eb4b3c379a55e983ca
isyouyoung <isyouyoung@gmail.com>
2026-07-06 14:32:30 +0900

    ModelRepository 생성 및 역할 주석 추가

---
+++ src/main/java/com/smartrig/repository/ModelRepository.java
@@ -0,0 +1,8 @@
+package com.smartrig.repository;
+
+// Repository는 DB와 직접 통신하는 계층이다.
+// Entity를 저장, 조회, 수정, 삭제하는 역할을 담당한다.
+// Service는 Repository를 통해 DB에 접근한다.
+public interface ModelRepository {
+
+}
