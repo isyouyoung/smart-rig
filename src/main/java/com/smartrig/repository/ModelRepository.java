57a588b5d8450218639015b0ecb8bb04355386a9
isyouyoung <isyouyoung@gmail.com>
2026-07-06 14:44:03 +0900

    ModelRepository에 JpaRepository 상속 및 역할 주석 추가

--- src/main/java/com/smartrig/repository/ModelRepository.java
+++ src/main/java/com/smartrig/repository/ModelRepository.java
@@ -1,8 +1,13 @@
 package com.smartrig.repository;
 
+import com.smartrig.repository.entity.ModelEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 // Repository는 DB와 직접 통신하는 계층이다.
-// Entity를 저장, 조회, 수정, 삭제하는 역할을 담당한다.
 // Service는 Repository를 통해 DB에 접근한다.
-public interface ModelRepository {
+// Entity를 저장, 조회, 수정, 삭제하는 역할을 담당한다.
+// Spring Data JPA의 JpaRepository를 상속받아 기본 CRUD 기능을 자동으로 사용할 수 있다.
+// ModelEntity를 관리하며, 기본키(PK) 타입은 Long이다.
+public interface ModelRepository extends JpaRepository<ModelEntity, Long> {
 
 }
