ef6b1fb973b12a56b33858da0903f490db1f2d27
isyouyoung <isyouyoung@gmail.com>
2026-07-07 23:01:21 +0900

    ModelService 구현체 생성

---
+++ src/main/java/com/smartrig/service/impl/ModelService.java
@@ -0,0 +1,9 @@
+package com.smartrig.service.impl;
+
+import com.smartrig.service.IModelService;
+
+// IModelService를 구현하는 Service 클래스이다.
+// 실제 비즈니스 로직을 작성하는 계층이다.
+public class ModelService implements IModelService {
+
+}
