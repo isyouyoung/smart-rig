142abd02c2c05b3d3ff9ad8a9a1321c59ed81061
isyouyoung <isyouyoung@gmail.com>
2026-07-08 23:16:02 +0900

    ModelService saveModel 메서드 구현 추가

--- src/main/java/com/smartrig/service/impl/ModelService.java
+++ src/main/java/com/smartrig/service/impl/ModelService.java
@@ -1,5 +1,6 @@
 package com.smartrig.service.impl;
 
+import com.smartrig.repository.entity.ModelEntity;
 import com.smartrig.service.IModelService;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +8,12 @@ import org.springframework.stereotype.Service;
 // 실제 비즈니스 로직을 작성하는 계층이다.
 @Service
 public class ModelService implements IModelService {
+
+    // ModelEntity 저장 기능을 구현하는 메서드이다.
+    // 실제 DB 저장은 Repository를 통해 수행한다.
+    @Override
+    public void saveModel(ModelEntity modelEntity) {
+
+    }
 
 }
