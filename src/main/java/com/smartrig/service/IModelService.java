c15fc3b32334a6131d8f743c019daf8b7a47c0ba
isyouyoung <isyouyoung@gmail.com>
2026-07-08 23:10:35 +0900

    IModelService에 saveModel 메서드 선언 추가

--- src/main/java/com/smartrig/service/IModelService.java
+++ src/main/java/com/smartrig/service/IModelService.java
@@ -1,6 +1,12 @@
 package com.smartrig.service;
 
+import com.smartrig.repository.entity.ModelEntity;
+
 // Model 관련 비즈니스 로직을 정의하는 Service 인터페이스이다.
 public interface IModelService {
+
+    // ModelEntity 객체를 저장하기 위한 메서드이다.
+    // 실제 저장 로직은 구현 클래스(ModelService)에서 작성한다.
+    void saveModel(ModelEntity modelEntity);
 
 }
