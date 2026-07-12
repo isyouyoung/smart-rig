e249981ca4e588ddba919e26e8881aa4bdcd7e94
BLUE <isyouyoung@gmail.com>
2026-07-13 00:08:04 +0900

    이해 + 주석 설명 추가 완료

--- src/main/java/com/smartrig/controller/ModelController.java
+++ src/main/java/com/smartrig/controller/ModelController.java
@@ -19,15 +19,14 @@ public class ModelController {
     public ModelController(IModelService modelService) {
         this.modelService = modelService;
     }
-
-//    @PostMapping("/saveModel")
-//    public void saveModel() {
-//    }
-
+    
     // Model 저장 요청을 처리하는 API이다.
     @PostMapping("/saveModel")
+    // 위 코드에 RequestMapping을 기본주소에 추가로 /model/v1/saveModel 가 됨
     public void saveModel(ModelEntity modelEntity) {
+        // 클라이언트로부터 modelEntity 객체를 매개변수로 받겠다는 뜻
         modelService.saveModel(modelEntity);
+        // 전달받은 modelEntity를 Service한태 저장하라고 요청
     }
 
 }
