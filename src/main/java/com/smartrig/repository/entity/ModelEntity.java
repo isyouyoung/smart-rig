290fb1bf6166362858d2ecfc6f5ff7dd849d826d
isyouyoung <isyouyoung@gmail.com>
2026-07-22 23:48:55 +0900

    주석 설명 추가 (@Id // 이 필드가 Primary Key(PK) 입니다를 의미)

--- src/main/java/com/smartrig/repository/entity/ModelEntity.java
+++ src/main/java/com/smartrig/repository/entity/ModelEntity.java
@@ -27,7 +27,7 @@ public class ModelEntity {
     // JPA는 이 값을 기준으로 어떤 데이터를 조회/수정/삭제할지 판단한다.
     // DB의 BIGINT 타입과 매핑된다.
     // Long을 사용하는 이유는 저장 전에는 ID가 없어 null 상태가 될 수 있기 때문이다.
-    @Id
+    @Id // 이 필드가 Primary Key(PK) 입니다를 의미
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "model_id") // 컬럼 추가 이해하기 쉬움 Java 필드명은 modelId 그대로 사용하지만 DB 컬럼명은 model_id를 사용해라 라는뜻
     // Column 추가후 MODEL 조회 API 테스트 성공함!
