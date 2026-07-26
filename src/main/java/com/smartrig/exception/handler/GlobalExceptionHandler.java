d65f8a67a8fb030bb1847d9d7c47efe7baf96f5e
isyouyoung <isyouyoung@gmail.com>
2026-07-27 01:02:15 +0900

    예외 응답을 ErrorResponseDTO 기반으로 변경

--- src/main/java/com/smartrig/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/smartrig/exception/handler/GlobalExceptionHandler.java
@@ -5,16 +5,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.smartrig.dto.ErrorResponseDTO;
 
 @RestControllerAdvice // "모든 Controller에서 발생하는 예외를 내가 처리할게." 라고 Spring에게 알려주는 어노테이션이다.
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ModelNotFoundException.class) // "ModelNotFoundException이 발생하면 이 메서드를 실행해."
-    public ResponseEntity<String> handleModelNotFoundException(ModelNotFoundException e) {
+    public ResponseEntity<ErrorResponseDTO> handleModelNotFoundException(ModelNotFoundException e) {
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(e.getMessage());
+                .body(new ErrorResponseDTO(
+                        HttpStatus.NOT_FOUND.value(),
+                        e.getMessage()
+                ));
     }
 
 }
