10a98a4f2c365d74fdd608e765ad460540d2b322
isyouyoung <isyouyoung@gmail.com>
2026-08-01 00:25:25 +0900

    SmartRig Inventory 메인 화면 이동 버튼 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,20 @@
-console.log("SmartRig JS 연결 성공");
+
+$(document).ready(function () {
+
+    // 재고 조회 버튼
+    $("#btnSearchStock").on("click", function () {
+
+        console.log("조회 버튼 클릭");
+
+    });
+
+
+
+    // 메인 화면 이동
+    $("#btnHome").on("click", function () {
+
+        location.href = "/html/index.html";
+
+    });
+
+});
